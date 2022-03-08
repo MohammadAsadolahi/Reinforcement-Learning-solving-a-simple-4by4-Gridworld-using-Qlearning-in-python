ce42bbfd1f7f1692445467d50a7bcb728cbd43a3
Mohammad Asadolahi <79268727+mohammadAsadolahi@users.noreply.github.com>
2022-03-08 02:52:00 -0800

    result plot comands added

--- Reinforcement_Learning_solving_a_simple_4_4_Gridworld_using_Q_learning.py
+++ Reinforcement_Learning_solving_a_simple_4_4_Gridworld_using_Q_learning.py
@@ -101,23 +101,39 @@ policy = env.getRandomPolicy()
 # policy = {(0, 0): 'R', (0, 1): 'R', (0, 2): 'D', (0, 3): 'L', (1, 0): 'U', (1, 1): 'R', (1, 2): 'D', (1, 3): 'D'
 #     ,(2, 0): 'D', (2, 1): 'R', (2, 2): 'R', (2, 3): 'D', (3, 0): 'R', (3, 1): 'R', (3, 2): 'R'}
 # env.printPolicy(policy)
-
+averageRewards=[]
+rewards=[]
 alpha=0.1
-for i in range(2000):
+for i in range(1,2002):
     state = env.reset()
     stepCounts=0
+    episodeReward=0
     while (not env.is_terminal(state)) and (stepCounts<20):
         action, nextState, reward = env.move(state, policy,0.01)
         stepCounts += 1
         targetQ=reward
+        episodeReward+=reward
         if not env.is_terminal(nextState):
           targetQ=reward+(0.9*env.qTable[nextState][max(env.qTable[nextState], key=env.qTable[nextState].get)])
         env.qTable[state][action]=env.qTable[state][action]+alpha*(targetQ-env.qTable[state][action])
         state = nextState
+    rewards.append(episodeReward)
+    averageRewards.append(sum(rewards)/i)
     for state in policy:
       policy[state] = max(env.qTable[state], key=env.qTable[state].get)
-    if (i+1)%200==0:
-        print(f"\n\n\n step:{i}")
+    if (i-1)%200==0:
+        print(f"\n\n\n step:{i-1} - Average reward so far:{sum(rewards)/i}")
+        print(f"\n\n\n step:{i-1}")
         env.printPolicy(policy)
         print("\n")
 print(f"exploited:{env.exploited}  explored:{env.explored}")
+
+plt.title(f'Total Rewards')
+plt.yscale('symlog')
+plt.plot(rewards)
+plt.savefig("Total Rewards",dpi=200)
+plt.clf()
+plt.title(f'Average Rewards')
+plt.yscale('symlog')
+plt.plot(averageRewards)
+plt.savefig("Average Rewards",dpi=200)
